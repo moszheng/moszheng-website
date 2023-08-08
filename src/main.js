1d72e704ce135642aabf7492c7cb29b7b832bf79
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-09 07:08:32 +0800

    NotFound.vue

--- src/main.js
+++ src/main.js
@@ -9,13 +9,15 @@ import Works from './views/Works.vue'
 import WorksPage from './components/WorksPage.vue'
 import About from './views/About.vue'
 import Contact from './views/Contact.vue'
+import NotFound from './views/NotFound.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/works', component: Works },
     { path: '/works/:projecturl', name:'WorksPage',component: WorksPage, props: true},
     { path: '/about', component: About },
-    { path: '/contact', component: Contact }
+    { path: '/contact', component: Contact },
+    { path: '/:catchAll(.*)', component: NotFound },
 ];
 
 // Router method
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+<main class="text-center">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+</main>
+</template>
