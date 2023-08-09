2bed8a38fd404a5818e6368552735882c42ce107
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-09 17:59:45 +0800

    WorksPage Fetching Data/ Add Data Info/ svg move

--- src/components/WorksPage.vue
+++ src/components/WorksPage.vue
@@ -1,31 +1,44 @@
-<script>
+<script setup>
 import Footer from './Footer.vue'
-export default{
-    props: ['projecturl'],
-}
+import WorksData from '../data/WorksData.json'
+import { ref } from 'vue'
+
+const props = defineProps({
+    projecturl: String
+})
+
+const prjdata = WorksData.project.find(item => item.name == props.projecturl);
+
 </script>
 <template>
 <main class="container">
     <div class="ratio ratio-16x9 mb-5">
-        <iframe src="https://player.vimeo.com/video/276425004?h=6ea64f06ea&color=ffffff&title=0&byline=0&portrait=0"
-            allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-        
+        <iframe :src=prjdata.video allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
     </div>
     <div class="row px-md-5">
         <div class="col-md-5">
-            <!-- {{ $router.params.projecturl }} -->
-            <h3>{{ projecturl }}</h3>
-            <h6>@company</h6>
-            <p>date</p>
-            <p>www.behanceurl.com</p>
+            <h3>{{ projecturl}}</h3>
+            <h6>@{{prjdata.company}}</h6>
+            <p>{{prjdata.date}}</p>
+            <a class="" :href=prjdata.behance target="_blank" rel="noopener">
+                <svg id="icon_twitter">
+                    <use xlink:href="#icon-behance"></use>
+                </svg>
+            </a>
+            <a class="" :href=prjdata.behance target="_blank" rel="noopener">
+                <svg id="icon_twitter">
+                    <use xlink:href="#icon-behance"></use>
+                </svg>
+            </a>
         </div>
         <div class="col-md-7">
-            <p>
-            The Golden Melody Award (GMA) is Taiwan's most prominent music ceremony, and it holds a special place in my heart as it has been my usual project for five times since 2018 when I was a student and took an Internship at JL Design.
-            </p>
-            <p>
-            During the three-month period leading up to the ceremony, we worked diligently to create the main visual, which included several elements aimed at producing a captivating TV package. It was a challenging yet rewarding experience, and each year, the project allowed me to grow both personally and professionally.
-            </p>
+            <div class="mb-5">
+                <p>The Golden Melody Award (GMA) is Taiwan's most prominent music ceremony, and it holds a special place in my heart as it has been my usual project for five times since 2018 when I was a student and took an Internship at JL Design.</p>
+                <p>During the three-month period leading up to the ceremony, we worked diligently to create the main visual, which included several elements aimed at producing a captivating TV package. It was a challenging yet rewarding experience, and each year, the project allowed me to grow both personally and professionally.</p>
+            </div>
+            <div>
+                <h5>Credit</h5>
+            </div>
         </div>
     </div>
 </main>
