568a7c834dd8e6bd6412bbcda9daf0a1b442c633
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-17 18:13:02 +0800

    Delete Works Filter

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -6,36 +6,9 @@ import WorksData from '../data/WorksData.json'
 </script>
 <template>
 <div>
-    <main class="row flex-xl-nowrap">
-		<!-- Columns filter Bar -->
-		<aside class="col col-xl-1 bd-sidebar bg-white">
-			<ul class="list-unstyled ps-0">
-				<li class="my-2">
-				<div class="mt-4 mb-3">
-					<button class="btn btn-toggle align-items-center rounded collapsed advance-filter-button" 
-					data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-					<svg id="icon_filter">
-						<use xlink:href="#icon-filter-category"></use>
-					</svg>
-					<span class="t-bold">Works </span>
-					</button>
-				</div>
-				<!-- Filters -->
-				<div class="collapse" id="orders-collapse">
-					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-3">
-						<li v-for="item in WorksData.project" class="mb-2">
-							<a :href=item.url title="Link to project" target="_blank" rel="noopener" >
-								{{item.name}}
-							</a>
-						</li>
-					</ul>
-				</div>
-				</li>
-				<li class="border-top"></li>
-			</ul>
-		</aside>
+    <main class="flex-xl-nowrap">
 		<!----- Main Content----->
-		<div class="col-xl-11 py-md-3 pl-md-5 px-xl-3 bd-content">
+		<div class="py-md-3 pl-md-5 px-xl-3 bd-content">
 			<div class="row">
 				<div  v-for="item in WorksData.project" class="col-md-4">
 					<div class="card mb-4 text-white">
