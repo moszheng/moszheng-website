7d5012b4142ddb260f029f2ba41d2821f9755d18
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-23 00:00:46 +0800

    add + button to nav bar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import LinkData from './data/LinkData.json'
 import LoadingPage from "./views/Loading.vue";
 
+// const socialbar 
 </script>
 <template>
 <header class="sticky-top">
@@ -29,7 +30,12 @@ import LoadingPage from "./views/Loading.vue";
                         <li class="nav-item"><router-link :to="{ name : 'About' }" class="nav-link link-dark px-2">About</router-link></li>
                         <li class="nav-item"><router-link :to="{ name : 'Contact' }" class="nav-link link-dark px-2">Contact</router-link></li>
                     </ul>
-                    <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                    <button class="btn justify-content-around" type="button" @click="socialbar">
+                        <svg id="icon_twitter">
+                            <use xlink:href="#icon-plus"></use>
+                        </svg>
+                    </button>
+                    <!-- <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
                         <li v-for="item in LinkData.socialmedia" class="nav-item col-3 col-md-auto">
                             <a class="nav-link p-2" :href="item.url" target="_blank" rel="noopener">
                                 <svg id="icon_twitter">
@@ -37,7 +43,7 @@ import LoadingPage from "./views/Loading.vue";
                                 </svg>
                             </a>
                         </li>
-                    </ul>
+                    </ul> -->
                 </div>
              </div>
         </div>
@@ -93,6 +99,9 @@ import LoadingPage from "./views/Loading.vue";
         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
     </symbol>
     <!-- other -->
+    <symbol id="icon-plus" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+    </symbol>
     <symbol id="bi-envelope-fill" viewBox="0 0 16 16" class="bi bi-envelope-fill">
         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
     </symbol>
