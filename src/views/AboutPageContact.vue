6cbd1c9ac3e95395be11f3ba40b34a2fe91154ff
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2024-02-04 12:06:02 +0800

    [ CSS ] About Contact

--- src/views/AboutPageContact.vue
+++ src/views/AboutPageContact.vue
@@ -8,16 +8,16 @@ const splitText = (string) => {
 </script>
 <template>
     <section class="about-contact d-flex-center min-h-[80vh] w-full bg-main-black px-4 py-8 lg:px-5">
-        <div class="container flex lg:justify-around flex-col lg:flex-row">
+        <div class="container flex flex-col lg:flex-row lg:justify-around">
             <!-- Links -->
-            <div class="px-0 md:px-12 xl:flex xl:px-0 lg:mb-0 mb-12">
+            <div class="mb-12 px-0 md:px-12 lg:mb-0 xl:flex xl:px-0">
                 <div class="about-intro-social">
-                    <div class="fill-white mb-10">
+                    <div class="mb-10 fill-white">
                         <svg class="w-54 h-16" id="mos-logo">
                             <use xlink:href="#icon-mosLogo"></use>
                         </svg>
                     </div>
-                    <ul class="list-reset mobile-center mb-10 flex flex-row flex-wrap">
+                    <ul class="list-reset mb-5 flex flex-row flex-wrap lg:mb-10">
                         <li v-for="item in LinkData.socialmedia" :key="item" class="intro-infosocial w-1/5">
                             <a class="inline-block rounded-full p-6 md:p-2" :href="item.url" target="_blank" rel="noopener">
                                 <svg class="size-7 fill-main-neutral lg:size-9" id="icon_social">
@@ -26,13 +26,13 @@ const splitText = (string) => {
                             </a>
                         </li>
                     </ul>
-                    <div class="mobile-center">
-                        <div class="mb-4">
-                            <div v-for="(item, index) in LinkData.profile_link" :key="item.text" :data-index="index" class="intro-infocontact mb-2 flex p-2">
-                                <svg class="size-6 fill-main-neutral lg:size-7" id="icon_social">
+                    <div class="flex justify-center lg:justify-start">
+                        <div class="space-y-2">
+                            <div v-for="(item, index) in LinkData.profile_link" :key="item.text" :data-index="index" class="intro-infocontact flex items-center p-2">
+                                <svg class="mr-5 size-6 fill-main-neutral lg:size-7" id="icon_social">
                                     <use :xlink:href="item.icon"></use>
                                 </svg>
-                                <h5 class="ml-3 text-main-neutral">{{ item.text }}</h5>
+                                <h5 class="text-main-neutral">{{ item.text }}</h5>
                             </div>
                         </div>
                     </div>
@@ -40,14 +40,14 @@ const splitText = (string) => {
             </div>
             <!-- Button -->
             <div class="">
-                <h1 class="mb-12 uppercase text-main-neutral">
+                <h1 class="mb-8 uppercase text-main-neutral lg:mb-12">
                     <span v-for="(text, index) in splitText('Let\'s Get in Touch')" :key="index" class="inline-flex pe-2 xl:pe-4">
                         <span class="block font-bold" id="contact-title"> {{ text }} </span>
                     </span>
                 </h1>
-                <router-link :to="{ name: 'Contact' }" class="d-flex-center me-md-5 max-w-xl rounded-full border-4 border-main-neutral bg-transparent px-2 py-5">
+                <router-link :to="{ name: 'Contact' }" class="d-flex-center me-md-5 max-w-xl rounded-full border-4 border-main-neutral bg-transparent px-2 py-1 lg:py-5">
                     <h3 class="text-main-neutral">Contact</h3>
-                    <svg class="w-48 fill-main-neutral">
+                    <svg class="lg:24 h-24 w-36 fill-main-neutral lg:w-48">
                         <use xlink:href="#icon-arrowxl"></use>
                     </svg>
                 </router-link>
