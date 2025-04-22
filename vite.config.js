03c9a864f5081f99d0f834511af71fb484aace6c
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2025-04-22 14:45:14 +0800

    [install] tailwindcss/vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,16 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-
-    // build: {
-    //   rollupOptions: {
-    //     // https://rollupjs.org/guide/en/#outputmanualchunks
-    //     output: {
-    //       manualChunks: {
-    //         'group-user': [
-    //           './src/views/Home.vue',
-    //           './src/views/Works.vue',
-    //           './src/views/Contact.vue',
-    //         ],
-    //       },
-    //     },
-    //   },
-    // },
 });
